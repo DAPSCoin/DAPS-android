6b2ca7e7f36fd4d7aeb9154e445ea601c4a165bd
powerdev0510 <sun.han510@gmail.com>
2019-10-16 10:10:21 -0400

    Updated by Asai 1016

--- app/src/main/java/pivx/org/pivxwallet/ui/node_activity/NodeActivity.java
+++ app/src/main/java/pivx/org/pivxwallet/ui/node_activity/NodeActivity.java
@@ -1,17 +1,27 @@
 package pivx.org.pivxwallet.ui.node_activity;
 
+import android.app.Dialog;
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,12 +57,16 @@ public class NodeActivity extends Fragment implements View.OnClickListener, IOnF
     private EditText edit_name, edit_host, edit_port, edit_user, edit_password;
     private NodeAdapter nodeAdapter;
 
+    Dialog addNodeDialog;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         pivxApplication = PivxActivity.pivxApplication;
         pivxModule = PivxActivity.pivxModule;
         daps = PivxActivity.daps;
 
-        View root = inflater.inflate(R.layout.fragment_node_settings, container, false);
+        View root = inflater.inflate(R.layout.fragment_node_settings_new, container, false);
+        setupView(root);
+        /*View root = inflater.inflate(R.layout.fragment_node_settings, container, false);
 
         edit_node = (ExpandableListView) root.findViewById(R.id.address_node);
         edit_name = (EditText) root.findViewById(R.id.edit_name);
@@ -68,16 +82,72 @@ public class NodeActivity extends Fragment implements View.OnClickListener, IOnF
         NodeActivity.this.edit_host.setText(appConf.getCurNodeInfo().host);
         NodeActivity.this.edit_port.setText(String.valueOf(appConf.getCurNodeInfo().port));
         NodeActivity.this.edit_user.setText(appConf.getCurNodeInfo().user);
-        NodeActivity.this.edit_password.setText(appConf.getCurNodeInfo().password);
+        NodeActivity.this.edit_password.setText(appConf.getCurNodeInfo().password);*/
 
         return root;
+    }
+
+    private void setupView(View root) {
+        final String []types = new String[2];
+        types[0] = "140.82.34.78";
+        types[1] = "140.82.34.79";
+
+        Spinner nodeSpinner = (Spinner) root.findViewById(R.id.node_spinner);
+        final ArrayAdapter<String> nodeSpinnerAdapter = new ArrayAdapter<String>(getActivity(), R.layout.item_spinner, types);
+        nodeSpinner.setAdapter(nodeSpinnerAdapter);
+        nodeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Button addNodeBt = root.findViewById(R.id.button1);
+        addNodeBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddNodeDialog();
+            }
+        });
+
+        Button dnsDiscoverBt = root.findViewById(R.id.button2);
+        dnsDiscoverBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), DnsDiscoveryActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void showAddNodeDialog() {
+        addNodeDialog = new Dialog(getActivity());
+        addNodeDialog.requestWindowFeature(getActivity().getWindow().FEATURE_NO_TITLE);
+        addNodeDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        addNodeDialog.setContentView(R.layout.dialog_add_node);
+        addNodeDialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+        addNodeDialog.getWindow().setGravity(Gravity.CENTER);
+        addNodeDialog.show();
+
+        Button cancelBt = addNodeDialog.findViewById(R.id.button2);
+        cancelBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNodeDialog.dismiss();
+            }
+        });
     }
 
     @Override
     public void onResume() {
         super.onResume();
 
-        if(getActivity().getCurrentFocus()!=null) {
+        /*if(getActivity().getCurrentFocus()!=null) {
             InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(INPUT_METHOD_SERVICE);
             inputMethodManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
         }
@@ -128,7 +198,7 @@ public class NodeActivity extends Fragment implements View.OnClickListener, IOnF
                     return false;
                 }
             });
-        }
+        }*/
     }
 
     @Override
@@ -166,7 +236,7 @@ public class NodeActivity extends Fragment implements View.OnClickListener, IOnF
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
-        edit_node.setIndicatorBounds(edit_node.getWidth()- convertDpToPx(40), edit_node.getWidth());
+        //edit_node.setIndicatorBounds(edit_node.getWidth()- convertDpToPx(40), edit_node.getWidth());
     }
 
     private void showErrorDialog(String message) {
